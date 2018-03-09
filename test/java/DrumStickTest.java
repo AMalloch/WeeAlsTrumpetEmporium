10eee1cba040bb9e483e758b9069b68e370fc2b9
Angus Malloch <angusbass@hotmail.com>
2018-03-09 15:52:54 +0000

    Tested can get drum stick retail price.

--- test/java/DrumStickTest.java
+++ test/java/DrumStickTest.java
@@ -1,6 +1,9 @@
 import Classes.DrumStick;
 import ENUMs.DrumStickType;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class DrumStickTest {
 
@@ -9,5 +12,10 @@ public class DrumStickTest {
     @Before
     public void setUp() throws Exception {
         drumStick = new DrumStick("Vic Furth", 30, 20, 5, DrumStickType.BRUSHES);
+    }
+
+    @Test
+    public void canGetRetailPrice(){
+        assertEquals(30, drumStick.getRetailPrice());
     }
 }
